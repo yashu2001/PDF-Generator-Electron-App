1f63518f4a3653f10dd4a9261ca8fa0c2a099cde
Yash.S.Narang <y_narang@yahoo.in>
2021-01-12 17:43:47 +0530

    Updating initial data and production ready paths

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ function createWindow(minHeight, minWidth, uri) {
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true,
+      webSecurity: false,
     },
     show: false,
     minHeight: minHeight,
@@ -35,22 +36,42 @@ function createWindow(minHeight, minWidth, uri) {
       await DB.createUser({
         username: "admin",
         role: "admin",
-        password: "admin",
+        password: "admin4545",
       });
       await DB.createUser({
-        username: "user",
+        username: "oswin",
         role: "user",
-        password: "user",
+        password: "1234",
       });
       await DB.createUser({
         username: "root",
         role: "super-admin",
         password: "toor",
       });
+      await DB.createTemplate({
+        name: "Label-1",
+        document_height: 248,
+        document_width: 165,
+        font_size: 11,
+        document_orientation: "portrait",
+        coordinates: [
+          { x: "25", y: "89" },
+          { x: "100", y: "89" },
+          { x: "25", y: "205" },
+          { x: "100", y: "205" },
+        ],
+      });
     }
     app
       .whenReady()
-      .then(createWindow.bind(null, 250, 200, "./views/Login.html"));
+      .then(
+        createWindow.bind(
+          null,
+          250,
+          200,
+          path.join(__dirname, "./views/Login.html")
+        )
+      );
   } catch (err) {
     console.log(err);
   }
@@ -66,7 +87,7 @@ app.on("window-all-closed", () => {
 
 app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow(250, 200, "./views/Login.html");
+    createWindow(250, 200, path.join(__dirname, "./views/Login.html"));
   }
 });
 
@@ -74,18 +95,24 @@ app.on("activate", () => {
 ipcMain.on("auth", (e, args) => {
   if (args.role === "super-admin") {
     // Load super-admin view
-    createWindow(550, 700, "./views/root.html");
+    createWindow(550, 700, path.join(__dirname, "./views/root.html"));
   } else if (args.role === "admin") {
     // Load admin view
-    createWindow(550, 700, "./views/adminOptions.html");
+    createWindow(550, 700, path.join(__dirname, "./views/adminOptions.html"));
   } else {
     // Load User view
-    createWindow(550, 700, "./views/existingTemplate.html");
+    createWindow(
+      550,
+      700,
+      path.join(__dirname, "./views/existingTemplate.html")
+    );
   }
   BrowserWindow.getAllWindows()[1].close();
 });
 
 // Admin Listener
 ipcMain.on("admin", (e, args) => {
-  BrowserWindow.getAllWindows()[0].loadFile(`./views/${args.screen}`);
+  BrowserWindow.getAllWindows()[0].loadFile(
+    path.join(__dirname, `./views/${args.screen}`)
+  );
 });
